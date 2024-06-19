8c2cd67606c4027a01da3cd8e744d04c2a772b4d
fp024 <aopenasus@naver.com>
2024-06-19 18:22:47 +0900

    예제 2-3  파싱 로직을 추출해 한 클래스로 만듦

---
+++ chap02/src/main/java/org/rwsd/study/domain/BankTransaction.java
@@ -0,0 +1,46 @@
+package org.rwsd.study.domain;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import lombok.Getter;
+
+@Getter
+public class BankTransaction {
+  private final LocalDate date;
+  private final double amount;
+  private final String description;
+
+  public BankTransaction(LocalDate date, double amount, String description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+
+  @Override
+  public String toString() {
+    return "BankTransaction{"
+        + "date="
+        + date
+        + ", amount="
+        + amount
+        + ", description='"
+        + description
+        + '\''
+        + '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    BankTransaction that = (BankTransaction) o;
+    return Double.compare(that.amount, amount) == 0
+        && date.equals(that.date)
+        && description.equals(that.description);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(date, amount, description);
+  }
+}
---
+++ chap02/src/test/java/org/rwsd/study/BankStatementCSVParserTests.java
@@ -0,0 +1,34 @@
+package org.rwsd.study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.rwsd.study.domain.BankTransaction;
+
+class BankStatementCSVParserTests {
+
+  private BankStatementCSVParser parser = new BankStatementCSVParser();
+
+  @Test
+  void testParseLinesFromCSV() {
+
+    List<BankTransaction> result =
+        parser.parseLinesFromCSV(List.of("30-01-2017,-100,Deliveroo", "30-01-2017,-50,Tesco"));
+
+    assertThat(result).hasSize(2);
+
+    assertThat(result.get(0).getDate()) //
+        .isEqualTo(LocalDate.of(2017, 1, 30));
+    assertThat(result.get(0).getAmount()) //
+        .isEqualTo(-100.0);
+    assertThat(result.get(0).getDescription()) //
+        .isEqualTo("Deliveroo");
+
+    assertThat(result.get(1)) //
+        .hasFieldOrPropertyWithValue("date", LocalDate.of(2017, 1, 30))
+        .hasFieldOrPropertyWithValue("amount", -50.0)
+        .hasFieldOrPropertyWithValue("description", "Tesco");
+  }
+}
